941d954bcb8762ee7e88cf01aee497063773726a
twosee <twose@qq.com>
2019-04-12 17:02:37 +0800

    Improve setContentData.

--- src/Record.php
+++ src/Record.php
@@ -126,8 +126,13 @@ class Record
      */
     public function setContentData(string $data): self
     {
-        $this->contentData = $data;
-        $this->contentLength = strlen($this->contentData);
+        $this->contentLength = strlen($data);
+        if ($this->contentLength > FastCGI::MAX_CONTENT_LENGTH) {
+            $this->contentLength = FastCGI::MAX_CONTENT_LENGTH;
+            $this->contentData = substr($data, 0, FastCGI::MAX_CONTENT_LENGTH);
+        } else {
+            $this->contentData = $data;
+        }
         $extraLength = $this->contentLength % 8;
         $this->paddingLength = $extraLength ? (8 - $extraLength) : 0;
         return $this;
